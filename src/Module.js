de3424be847c488b2aabc362511c8211c0b26b6f
maebo <maeboittiaux@gmail.com>
2024-06-20 16:46:30 +0100

    Set default value of input boxes to 0

--- src/Module.js
+++ src/Module.js
@@ -7,21 +7,21 @@ function Module() {
             <div className="module-row">
                 <div className="module-input">
                     <label htmlFor="coursework-grade">Coursework Grade :</label>
-                    <input type="number" id="coursework-grade" name="coursework-grade" />
+                    <input type="number" id="coursework-grade" name="coursework-grade" defaultValue="0" />
                 </div>
                 <div className="module-input">
                     <label htmlFor="coursework-weighting" className="weighting-label">Weighting :</label>
-                    <input type="number" id="coursework-weighting" name="coursework-weighting" />
+                    <input type="number" id="coursework-weighting" name="coursework-weighting" defaultValue="0" />
                 </div>
             </div>
             <div className="module-row">
                 <div className="module-input">
                     <label htmlFor="exam-grade">Examination Grade :</label>
-                    <input type="number" id="exam-grade" name="exam-grade" />
+                    <input type="number" id="exam-grade" name="exam-grade" defaultValue="0" />
                 </div>
                 <div className="module-input">
                     <label htmlFor="exam-weighting" className="weighting-label">Weighting :</label>
-                    <input type="number" id="exam-weighting" name="exam-weighting" />
+                    <input type="number" id="exam-weighting" name="exam-weighting" defaultValue="0" />
                 </div>
             </div>
         </div>
