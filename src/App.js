1a8bc781f9845c64ac2137380589cbb59753bfbe
maebo <maeboittiaux@gmail.com>
2024-06-20 14:21:02 +0100

    Refactored title its its own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Title from './Title';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Grade Calculator</h1>
+        <Title />
       </header>
     </div>
   );
